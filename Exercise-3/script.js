bb00f5b6766922b2b19d0d79fa340005be46947f
DavidDccs <david.castro001@edu.pucrs.br>
2022-10-14 09:17:31 -0300

    Made the program work with alert and a simple for loop

---
+++ Exercise-3/script.js
@@ -0,0 +1,10 @@
+const number = document.getElementById('numberMult')
+
+function multTable(){
+
+for(let i = 1; i<11; i++){
+  alert(number.valueAsNumber+ "x" + i +"=" + number.valueAsNumber*i);
+
+}
+
+}
